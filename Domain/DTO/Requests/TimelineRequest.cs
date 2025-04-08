55cc348bd39e4becb69811b371cf83a20428a4ed
bduchuy <huybdse173401@fpt.edu.vn>
2025-04-08 22:24:27 +0700

    fix create timeline date

--- Domain/DTO/Requests/TimelineRequest.cs
+++ Domain/DTO/Requests/TimelineRequest.cs
@@ -1,12 +1,18 @@
 using System;
+using System.Text.Json.Serialization;
 
 namespace Domain.DTO.Requests;
 
 public class TimelineRequest
 {
     public int? SequenceId { get; set; }
+    
+    [JsonConverter(typeof(JsonDateTimeConverter))]
     public DateTime? StartDate { get; set; }
+    
+    [JsonConverter(typeof(JsonDateTimeConverter))]
     public DateTime? EndDate { get; set; }
+    
     public string? Event { get; set; }
     public int? TimelineType { get; set; }
     public int? CreatedBy { get; set; }
