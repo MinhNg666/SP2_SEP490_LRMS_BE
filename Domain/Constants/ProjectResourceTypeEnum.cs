b774853b5aade8b406549f34d9228d3e6a8ac090
unknown <vietnhse140570@fpt.edu.vn>
2025-05-14 19:58:12 +0700

    add enum type for notification and project resource

---
+++ Domain/Constants/ProjectResourceTypeEnum.cs
@@ -0,0 +1,17 @@
+namespace Domain.Constants;
+
+public enum ProjectResourceTypeEnum
+{
+    Human    = 1,
+    Equipment = 2,
+    Software = 3,
+    Document = 4,
+    Material = 5,
+    Personnel = 6,
+    Facility = 7,
+    Travel = 8,
+    Conference = 9,
+    Journal = 10,
+    Dataset = 11,
+    Other = 12
+} 
